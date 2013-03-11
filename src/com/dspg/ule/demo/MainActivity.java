ceee99289467a82f630e089ede3db231c6509ee9
Edward Pang <edwardpang@edwardpang.com>
2013-03-11 16:36:34 +0800

    Add HAN device table

--- src/com/dspg/ule/demo/MainActivity.java
+++ src/com/dspg/ule/demo/MainActivity.java
@@ -5,6 +5,10 @@ import android.app.Activity;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.util.Log;
 import android.view.Menu;
+
+import android.widget.LinearLayout;
+//import android.widget.ScrollView;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -12,6 +16,8 @@ public class MainActivity extends Activity {
 	private static final String TAG = "MainActivity";
 	
 	private TextView mTextViewVersionName;
+	//private ScrollView mScrollViewHanDeviceTable;
+	private LinearLayout mScrollViewHanDeviceTableLayout;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,11 @@ public class MainActivity extends Activity {
 		
 		mTextViewVersionName = (TextView) findViewById(R.id.textViewVersionName);
 		mTextViewVersionName.setText (getVersionName());
+		
+		//mScrollViewHanDeviceTable = (ScrollView) findViewById(R.id.scrollViewHanDeviceTable);
+	    mScrollViewHanDeviceTableLayout = (LinearLayout) findViewById (R.id.scrollViewHanDeviceTableLayout);
+
+	    updateHanDeviceTable (5);
 	}
 
 	@Override
@@ -27,6 +38,22 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	private void updateHanDeviceTable (int n) {
+		for(int i = 0; i < n; i ++) {
+			TableRow row = new TableRow(this);
+			TextView tv1 = new TextView(this);
+			tv1.setText("#"+i);
+			
+			TextView tv2 = new TextView(this);
+			tv2.setText("Something");
+
+			row.addView(tv1);
+			row.addView(tv2);
+			
+			mScrollViewHanDeviceTableLayout.addView(row);
+		}
 	}
 	
 	private String getVersionName () {
